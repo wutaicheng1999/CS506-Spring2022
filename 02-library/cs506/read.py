f3f82541f4b3a4780a9057f4335c99ac67117628
Taicheng Wu <wutaicheng1999@gmail.com>
2022-02-09 17:44:06 -0500

    Lab2

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -1,6 +1,10 @@
+import csv
 def read_csv(csv_file_path):
     """
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    file = open(csv_file_path, "r")
+    l = []
+    for row in csv.reader(file):
+        l.append(row)
